7877dd438ab4d37dae1ddcd1157153095af1f655
David Lazea <davidlazeadumitru11@gmail.com>
2025-10-16 23:13:11 +0300

    Lecția 1 - variabile și tipuri de date

---
+++ 01_Basics/ex2_tipuri_de_date.py
@@ -0,0 +1,22 @@
+# nume=input("cum te cheama?")
+# varsta=int(input("ce varsta ai?"))
+# inaltime=float(input("ce inaltime ai?"))
+# print("salut",nume)
+# print("ai ",varsta, "de ani")
+# print("ai ",inaltime,"m inaltime")
+
+# a=int(input("primul nr: "))
+# b=int(input("al doilea nr: "))
+# suma=a+b
+# diferenta=a-b
+# produs=a*b
+# impartire=a/b
+# print(suma)
+# print(diferenta)
+# print(produs)
+# print(impartire)
+
+prenume=input("care este prenumele tau? ")
+nume=input("care este numele tau? ")
+nume_total=prenume+" "+nume
+print("numele tau este:", nume_total)
